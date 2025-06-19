f3782ebf9578db49cf17ac60c04109dfedb1dd4a
Sopln-ishan <23ishansekhar10d@gmail.com>
2025-06-19 14:01:12 +0530

    created a new interface for the profile environment

--- src/app/newProfiles/page.tsx
+++ src/app/newProfiles/page.tsx
@@ -41,7 +41,35 @@ const Profiles = () => {
     const svgStartY = startY - (svgRect?.top || 0);
     const svgEndY = endY - (svgRect?.top || 0);
 
-    const pathD = `M ${startX} ${svgStartY} ${startX} ${(svgEndY - svgStartY) / 2} L ${endX} ${(svgEndY - svgStartY) / 2} L ${endX} ${svgEndY}`;
+    let pathD: string;
+    if(activeId === 0) {
+      pathD = `
+        M ${startX} ${svgStartY - 100}
+        L ${startX} ${(svgStartY + svgEndY - 10) / 2}
+        Q ${startX} ${(svgStartY + svgEndY) / 2}, ${startX - 5} ${(svgStartY + svgEndY) / 2}
+        L ${endX + 5} ${(svgStartY + svgEndY) / 2}
+        Q ${endX} ${(svgStartY + svgEndY) / 2}, ${endX} ${(svgStartY + svgEndY + 10) / 2}
+        L ${endX} ${svgEndY}
+      `;
+    } 
+
+    if(activeId === 1) {
+      pathD = `
+        M ${startX} ${svgStartY - 100}
+        L ${endX} ${svgEndY}
+      `;
+    }
+
+    if(activeId === 2) {
+      pathD = `
+        M ${startX} ${svgStartY - 100}
+        L ${startX} ${(svgStartY + svgEndY - 10) / 2}
+        Q ${startX} ${(svgStartY + svgEndY) / 2}, ${startX + 5} ${(svgStartY + svgEndY) / 2}
+        L ${endX - 5} ${(svgStartY + svgEndY) / 2}
+        Q ${endX} ${(svgStartY + svgEndY) / 2}, ${endX} ${(svgStartY + svgEndY + 10) / 2}
+        L ${endX} ${svgEndY}
+      `;
+    }
 
     sethPathD(pathD);
 
@@ -55,34 +83,33 @@ const Profiles = () => {
   };
 
   const controls = useAnimation();
-  useEffect(() => {
-    controls.start({
-      strokeDashoffset: -pathLength - 200,
-      transition: { duration: 3, ease: "easeInOut" }
-    });
-  }, [pathLength]);
   
-  const handleEnter = (evt) => {
-    setActiveId(Number(evt.target.id));
+  useEffect(() => {
 
-    //start animation on hover
-    controls.set({ strokeDashoffset: 100 });
+    setTimeout(() => {
+      setActiveId((activeId + 1) % 3);
+    }, 3000);
+
+    controls.set({ strokeDashoffset: pathLength });
     controls.start({
       strokeDashoffset: -pathLength - 200,
-      transition: { duration: 3, ease: "easeInOut" }
-    })
-  };
-
-  const handleLeave = () => {
-    setActiveId(-1);
-    controls.set({ strokeDashoffset: 100 });
-  };
+      transition: { duration: 3, ease: "easeInOut" },
+    });
+  }, [activeId]);
 
   useEffect(() => {
     updatePath();
   }, [activeId]);
 
   useEffect(() => {
+    controls.start({
+      strokeDashoffset: -pathLength - 200,
+      transition: { duration: 3, ease: "easeInOut" }
+    });
+  }, [pathLength]);
+
+  useEffect(() => {
+    setActiveId(0);
     updatePath();
     window.addEventListener("resize", updatePath);
     return () => window.removeEventListener("resize", updatePath);
@@ -91,15 +118,23 @@ const Profiles = () => {
   
 
   return ( 
-    <div className='bg-black text-white min-h-screen flex flex-col items-center justify-center'>
-      <section ref={startRef}>
-        <IoPersonOutline size='48px' />
+    <div className='body bg-gradient-to-b from-#ff9900 via-#ffea00 to-#ffbb00 text-white min-h-screen flex flex-col items-center justify-center'>
+      <section ref={startRef} className='icon-hover p-10 rounded-xl hover:from-orange-400 hover:to-orange-600 text-white'>
+        <IoPersonOutline size='72px' />
       </section>
 
-      <svg width='[100vw]' height='400'>
+      <svg width='[100vw]' height='200'>
+        <defs>
+          {/* <linearGradient id="path-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stopColor="#00e5ff" />
+            <stop offset="50%" stopColor="#32fbe2" />
+            <stop offset="100%" stopColor="#00e5ff" />
+          </linearGradient> */}
+        </defs>
+
         <motion.path 
           d={pathD}
-          stroke="gray" 
+          stroke="#32fbe2" 
           strokeWidth="2"
           fill="none"
           strokeDasharray={`${activeId !== -1 ? pathLength : '0'} ${pathLength + 200}`}
@@ -111,32 +146,26 @@ const Profiles = () => {
       <section className='flex items-center justify-evenly w-full'>
         <section 
           ref={laptopRef}
-          className='icon p-4 shadow-md rounded-lg bg-gray-600 hover:bg-orange-400 scale-120 transition-bg duration-500'
-          onMouseEnter={handleEnter}
-          onMouseLeave={handleLeave}
+          className={`${activeId === 0 ? 'icon-hover' : 'icon'} p-10 rounded-xl hover:from-orange-400 hover:to-orange-600 text-white`}
           id="0"
         >
-          <Laptop2Icon size='48px' id="0" />
+          <Laptop2Icon size='72px' id="0" />
         </section>
 
         <section
           ref={documentRef}
-          className='icon p-4 shadow-md rounded-lg bg-gray-600 hover:bg-orange-400 scale-120 transition-bg duration-500 delay-3000'
-          onMouseEnter={handleEnter}
-          onMouseLeave={handleLeave}
+          className={`${activeId === 1 ? 'icon-hover' : 'icon'} p-10 rounded-xl hover:from-orange-400 hover:to-orange-600 text-white`}
           id="1"
         >
-          <TiDocumentText size='48px' id="1" />
+          <TiDocumentText size='72px' id="1" />
         </section>
 
         <section
           ref={coreRef}
-          className='icon p-4 shadow-md rounded-lg bg-gray-600 hover:bg-orange-400 scale-120 transition-bg duration-500 delay-3000'
-          onMouseEnter={handleEnter}
-          onMouseLeave={handleLeave}
+          className={`${activeId === 2 ? 'icon-hover' : 'icon'} p-10 rounded-xl hover:from-orange-400 hover:to-orange-600 text-white`}
           id="2"
         >
-          <FaGears size='48px' id="2" />
+          <FaGears size='72px' id="2" />
         </section>
       </section>
     </div>
