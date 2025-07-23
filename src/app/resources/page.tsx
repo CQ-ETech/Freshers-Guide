11b066cf976b117bfadb37c7a93edd268044e9a8
Himani Shah <himani.shah201205@gmail.com>
2025-07-23 17:02:48 +0530

    resources page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,8 +1,5 @@
 import Link from 'next/link'
 import GoldParticles from '../../components/GoldParticles';
-// Removed unused React hooks import
-
-const SHEET_LINK = "https://docs.google.com/spreadsheets/d/1vxOF12HoiI05V52l7ZJfJchKv36rGOSNdl_zJPZ2NMo/edit?gid=0#gid=0"
 
 export default function Resources() {
   return (
@@ -18,14 +15,32 @@ export default function Resources() {
       <p className="text-lg text-center max-w-xl mb-6">
         Here you'll find a curated sheet of all useful resources across all profiles. Bookmark it and refer to it regularly.
       </p>
-      <a
-        href={SHEET_LINK}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="inline-block bg-[#FFF8E4] hover:bg-[#FFBB00] font-semibold py-3 px-6 rounded-lg shadow transition text-[#150000]"
-      >
-        Open Resource Sheet
-      </a>
+      {/* Resources Table */}
+      <div className="overflow-x-auto w-full max-w-2xl mb-8">
+        <table className="min-w-full border border-[#FFBB00] rounded-lg overflow-hidden">
+          <thead>
+            <tr className="bg-[#FFBB00] text-[#181818]">
+              <th className="py-2 px-4 text-left">Module</th>
+              <th className="py-2 px-4 text-left">Link</th>
+            </tr>
+          </thead>
+          <tbody className="bg-[#232323]">
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Finance</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/folderview?id=1WoGx7oPXahvE76faj2uq5vygeeRGtyus" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Finance</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/drive/folders/18WEe443J2d-OyPiwQkt29KwKj9gF1T3A?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Consulting</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/drive/mobile/folders/1NLV_Db_SBpjleFt2vmlpmjst7gq9dYsB?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Zerodha Varsity</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/folderview?id=1_jOIFqHc7B4iFhjsxtIkJSbyGUWWWnvq" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Investopedia</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/open?id=1aYZmvtHI7GWk124gQrE4Mp07soYJdS6Y" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">CFA L1 Dec'17 Mind Maps</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="http://www.mediafire.com/file/zbbw0cqvc8d5wn5/FREE_CFA_MIND_MAPS_LEVEL_1-_2017.pdf" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Mediafire PDF</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">FRM L1 GARP</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/open?id=1JRtpT2ivEeCSp7lF0wRRJjhyZdGd6XEq" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Guesstimates</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/file/d/1BEjrfPaR7iYB_jubtaFixibX_AfqaFu1/view?usp=drivesdk" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">UPSC</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://docs.google.com/document/d/163lUL1Q0c7ai_5lb8MSDihIU8-BlUkZjD5bH3LjMvrY/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Doc</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Virtual Stock Markets</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/file/d/1-kFJK4NhE0IITkEQGLZhkd2s6cWHLnvr/view?usp=drivesdk" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Consulting Fundae</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/file/d/1oVSuqyKMbRwQDRepD-CaxQ63nbBDVv_M/view?usp=drivesdk" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">KGP Life Fundae</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/file/d/1kBlD7nzxGPMzzHP90bFhnTmnKHmdVo5M/view?usp=drivesdk" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+            <tr><td className="py-2 px-4 border-b border-[#FFBB00]/30">Resources by Magicmeets</td><td className="py-2 px-4 border-b border-[#FFBB00]/30"><a href="https://drive.google.com/folderview?id=11Pp9SWpVUUNKHCR0nGGoqiossLyli3Dm" target="_blank" rel="noopener noreferrer" className="text-[#FFD600] hover:underline">Google Drive</a></td></tr>
+          </tbody>
+        </table>
+      </div>
       
       <div className="mt-8">
         <Link href="/" className="text-[#FFBB00] hover:underline">
